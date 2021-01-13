e88a898b8d2e30389857a2e1c3193f99eccec108
888Sourav888 <77108272+888Sourav888@users.noreply.github.com>
2021-01-13 21:35:36 +0530

    Learning to edit a file
    One comment line is added to this python code

--- factorial.py
+++ factorial.py
@@ -2,3 +2,5 @@ def fact(n):
   return 1 if(n == 1) else n * fact(n-1) 
 
 print(fact(int(input("Enter the number for which you want to find the factorial:")))) 
+
+#This comment is typed while trying to edit a git file
